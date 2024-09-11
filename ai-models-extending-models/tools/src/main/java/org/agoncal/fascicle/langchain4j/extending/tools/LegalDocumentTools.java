8b90c4d0798c6b6236f2a2919faf2dd1a2db80f4
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-09-11 07:36:05 +0100

    Returns

--- ai-models-extending-models/tools/src/main/java/org/agoncal/fascicle/langchain4j/extending/tools/LegalDocumentTools.java
+++ ai-models-extending-models/tools/src/main/java/org/agoncal/fascicle/langchain4j/extending/tools/LegalDocumentTools.java
@@ -7,17 +7,17 @@ import java.time.LocalDate;
 // tag::adocSnippet[]
 public class LegalDocumentTools {
 
-  @Tool("The last time the PRIVACY document was updated")
+  @Tool("Returns the last time the PRIVACY document was updated")
   public LocalDate lastUpdatePrivacy() {
     return LocalDate.of(2013, 3, 9);
   }
 
-  @Tool("The last time the TERMS AND CONDITIONS document was updated")
+  @Tool("Returns the last time the TERMS AND CONDITIONS document was updated")
   public LocalDate lastUpdateTerms() {
     return LocalDate.of(2014, 6, 19);
   }
 
-  @Tool("The last time the END USER LICENSE AGREEMENT document was updated")
+  @Tool("Returns the last time the END USER LICENSE AGREEMENT document was updated")
   public LocalDate lastEndUserLicenseAgreement() {
     return LocalDate.of(2021, 11, 23);
   }
