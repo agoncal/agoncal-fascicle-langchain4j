a2c3c121a1e1714078d6c1baf513595142572895
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-09-25 14:37:50 +0200

    Stream

---
+++ simplifying-ai-ai-services/accessing-models-open-ai/src/main/java/org/agoncal/fascicle/langchain4j/simplifying/accessing/openai/MusicianAssistant.java
@@ -0,0 +1,16 @@
+package org.agoncal.fascicle.langchain4j.simplifying.accessing.openai;
+
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.TokenStream;
+import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
+
+// tag::adocSnippet[]
+public interface MusicianAssistant {
+
+  @SystemMessage("You are an expert in musicians biography")
+  @UserMessage("Write a short biography about {{musician}}.")
+  TokenStream generateMusicianBiography(@V("musician") String musicianName);
+
+}
+// end::adocSnippet[]
---
+++ simplifying-ai-ai-services/accessing-models-open-ai/src/main/java/org/agoncal/fascicle/langchain4j/simplifying/accessing/openai/MusicianService.java
@@ -0,0 +1,44 @@
+package org.agoncal.fascicle.langchain4j.simplifying.accessing.openai;
+
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.openai.OpenAiChatModelName;
+import dev.langchain4j.model.openai.OpenAiStreamingChatModel;
+import dev.langchain4j.model.output.Response;
+import dev.langchain4j.service.AiServices;
+import dev.langchain4j.service.TokenStream;
+
+import java.util.concurrent.CompletableFuture;
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+import static java.lang.System.exit;
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+public class MusicianService {
+
+  private static final String OPENAI_API_KEY = System.getenv("OPENAI_API_KEY");
+
+  public static void main(String[] args) {
+
+    // tag::adocSnippet[]
+    StreamingChatLanguageModel model = OpenAiStreamingChatModel.builder()
+      .apiKey(OPENAI_API_KEY)
+      .modelName(GPT_4_O_MINI)
+      .build();
+
+    MusicianAssistant assistant = AiServices.create(MusicianAssistant.class, model);
+
+    TokenStream tokenStream = assistant.generateMusicianBiography("John Coltrane");
+
+    tokenStream.onNext(System.out::print)
+      .onComplete(System.out::println)
+      .onError(Throwable::printStackTrace)
+      .start();
+    // end::adocSnippet[]
+  }
+}
