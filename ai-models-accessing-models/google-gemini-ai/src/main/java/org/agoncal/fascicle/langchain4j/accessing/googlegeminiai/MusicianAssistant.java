5f90f917f0a4f39349ab3c34b4dcc4965b61aac0
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-10-17 14:32:38 +0200

    Google AI Gemini

---
+++ ai-models-accessing-models/google-gemini-ai/src/main/java/org/agoncal/fascicle/langchain4j/accessing/googlegeminiai/MusicianAssistant.java
@@ -0,0 +1,39 @@
+package org.agoncal.fascicle.langchain4j.accessing.googlegeminiai;
+
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+
+// tag::adocSkip[]
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// end::adocSkip[]
+public class MusicianAssistant {
+
+  public static void main(String[] args) {
+    useGoogleAiGeminiChatModel();
+  }
+
+  private static final String GOOGLE_AI_GEMINI_API_KEY = "AIzaSyAcr2eeoVFbYIvAziIsA3vvr0v59gVi054";
+
+  // ###################################
+  // ### GOOGLE AI GEMINI CHAT MODEL ###
+  // ###################################
+  private static void useGoogleAiGeminiChatModel() {
+    System.out.println("### useGoogleAiGeminiChatModel");
+
+    // tag::adocSnippet[]
+    GoogleAiGeminiChatModel model = GoogleAiGeminiChatModel.builder()
+      .apiKey(GOOGLE_AI_GEMINI_API_KEY)
+      .modelName("gemini-1.5-flash")
+      // tag::adocSkip[]
+      .logRequestsAndResponses(true)
+      // end::adocSkip[]
+      .build();
+
+    System.out.println(model.generate("List some influential Jazz musicians"));
+    // end::adocSnippet[]
+  }
+}
