3ad9db825be4e94bd8a460fb8a54c1f48fb26df5
Antonio Goncalves <antonio.goncalves@gmail.com>
2024-12-30 18:49:46 +0100

    qwen

---
+++ ai-models-accessing-models/dashscope-qwen/src/main/java/org/agoncal/fascicle/langchain4j/accessing/dashscope/MusicianAssistant.java
@@ -0,0 +1,38 @@
+package org.agoncal.fascicle.langchain4j.accessing.dashscope;
+
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+
+// tag::adocSkip[]
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// end::adocSkip[]
+public class MusicianAssistant {
+
+  public static void main(String[] args) {
+    useQwenChatModelBuilder();
+  }
+
+  private static final String AWS_ACCESS_KEY_ID = System.getenv("AWS_ACCESS_KEY_ID");
+  private static final String AWS_SECRET_ACCESS_KEY = System.getenv("AWS_SECRET_ACCESS_KEY");
+
+  // ##############################
+  // ### BEDROCK LANGUAGE MODEL ###
+  // ##############################
+  private static void useQwenChatModelBuilder() {
+    System.out.println("### useQwenChatModelBuilder");
+
+    // tag::adocSnippet[]
+    QwenChatModel model = QwenChatModel.builder()
+      .temperature(0.9f)
+      .build();
+    // end::adocSnippet[]
+
+    String completion = model.generate("When was the first Beatles album released?");
+
+    System.out.println(completion);
+  }
+}
