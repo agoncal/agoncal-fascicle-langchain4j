947b6343c367f17488301c04f6900507214a9dbc
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-01-29 14:10:16 +0100

    Add DeepSeek

---
+++ ai-models-accessing-models/deepseek/src/main/java/org/agoncal/fascicle/langchain4j/accessing/deepseek/MusicianAssistant.java
@@ -0,0 +1,48 @@
+package org.agoncal.fascicle.langchain4j.accessing.deepseek;
+
+import dev.langchain4j.model.chat.ChatLanguageModel;
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
+import java.time.Duration;
+
+import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
+
+// tag::adocSkip[]
+
+/**
+ * @author Antonio Goncalves
+ * http://www.antoniogoncalves.org
+ * --
+ */
+// end::adocSkip[]
+public class MusicianAssistant {
+
+  private static final String DEEPSEEK_API_KEY = System.getenv("DEEPSEEK_API_KEY");
+
+  public static void main(String[] args) {
+    useOpenAIForDeepSeek();
+  }
+
+  // #############################
+  // ### DEEPSEEK CHAT MODEL ###
+  // #############################
+  static void useOpenAIForDeepSeek() {
+    System.out.println("### useOpenAIForDeepSeek");
+
+    // tag::adocUseOpenAIForDeepSeek[]
+    ChatLanguageModel model = OpenAiChatModel.builder()
+      .apiKey(DEEPSEEK_API_KEY)
+      .baseUrl("https://api.deepseek.com")
+      .modelName("deepseek-chat")
+      .temperature(1.3)
+      .timeout(Duration.ofSeconds(60))
+      .topP(2d)
+      .maxTokens(1000)
+      .build();
+
+    String answer = model.generate("When was the first Beatles album released?");
+
+    System.out.println(answer);
+    // end::adocUseOpenAIForDeepSeek[]
+  }
+}
