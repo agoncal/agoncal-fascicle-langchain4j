2c515e2c14055e370c1b59ddd123b2359cf06f8e
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-06-10 16:56:23 +0200

    Doc

--- ai-models-enriching-models/mcp-server/src/main/java/org/agoncal/fascicle/langchain4j/enriching/mcpserver/LegalDocumentMCPServer.java
+++ ai-models-enriching-models/mcp-server/src/main/java/org/agoncal/fascicle/langchain4j/enriching/mcpserver/LegalDocumentMCPServer.java
@@ -8,19 +8,22 @@ import io.quarkiverse.mcp.server.Tool;
 
 public class LegalDocumentMCPServer {
 
-  @Tool(name = "last_update_privacy", description = "Returns the last time the PRIVACY document was updated")
+  @Tool(name = "last_update_privacy",
+    description = "Returns the last time the PRIVACY document was updated")
   LocalDate lastUpdatePrivacy(McpLog log) {
     log.info("lastUpdatePrivacy");
     return LocalDate.of(2013, 3, 9);
   }
 
-  @Tool(name = "last_update_terms", description = "Returns the last time the TERMS AND CONDITIONS document was updated")
+  @Tool(name = "last_update_terms",
+    description = "Returns the last time the TERMS AND CONDITIONS document was updated")
   LocalDate lastUpdateTerms(McpLog log) {
     log.info("lastUpdateTerms");
     return LocalDate.of(2014, 6, 19);
   }
 
-  @Tool(name = "last_update_license_agreement", description = "Returns the last time the END USER LICENSE AGREEMENT document was updated")
+  @Tool(name = "last_update_license_agreement",
+    description = "Returns the last time the END USER LICENSE AGREEMENT document was updated")
   LocalDate lastEndUserLicenseAgreement(McpLog log) {
     log.info("lastEndUserLicenseAgreement");
     return LocalDate.of(2021, 11, 23);
