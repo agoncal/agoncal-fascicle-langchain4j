d2019e3eb4cd3c703b79e6060f05c736afb64289
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-06-13 21:13:32 +0200

    Review

--- first-steps-getting-started/src/test/java/org/agoncal/fascicle/langchain4j/gettingstarted/MusicianAssistantTest.java
+++ first-steps-getting-started/src/test/java/org/agoncal/fascicle/langchain4j/gettingstarted/MusicianAssistantTest.java
@@ -1,21 +1,18 @@
 package org.agoncal.fascicle.langchain4j.gettingstarted;
 
-import java.io.IOException;
-import java.time.Duration;
-import java.util.List;
-
-// tag::adocHeader[]
 import com.github.dockerjava.api.model.Image;
 import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.ollama.OllamaChatModel;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
 import org.testcontainers.DockerClientFactory;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.ollama.OllamaContainer;
 import org.testcontainers.utility.DockerImageName;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.List;
 
 @Testcontainers
 public class MusicianAssistantTest {
@@ -32,7 +29,8 @@ public class MusicianAssistantTest {
     ollamaContainer.start();
 
     ChatModel model = OllamaChatModel.builder()
-      .baseUrl(String.format("http://%s:%d", ollamaContainer.getHost(), ollamaContainer.getFirstMappedPort()))
+      .baseUrl(String.format("http://%s:%d", ollamaContainer.getHost(),
+                                             ollamaContainer.getFirstMappedPort()))
       .modelName(MODEL_NAME)
       .temperature(0.0)
       .timeout(Duration.ofMinutes(5))
@@ -56,7 +54,7 @@ public class MusicianAssistantTest {
       System.out.println("Creating a new Ollama container...");
       OllamaContainer ollama = new OllamaContainer(IMAGE_NAME);
       ollama.start();
-      System.out.println("Executing an 'ollama pull' command to pull the model...");
+      System.out.println("Executing an 'ollama pull' command...");
       ollama.execInContainer("ollama", "pull", MODEL_NAME);
       ollama.commitToImage(IMAGE_NAME);
       return ollama;
